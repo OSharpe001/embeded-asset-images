9188c2823ddb888733090bd5458b90811a068bea
O. Sharpe <osharpe001@gmail.com>
2022-12-02 10:41:08 -0500

    practice with React embeded assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import cat from './assets/images/cat.png';
 import './App.css';
 
 function App() {
+  const randomImageUrl = "https://picsum.photos/400/264";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task: Add three images, using three different 
+        methods</h1>
+      <img
+      height = {200}
+      src = {cat}
+      alt = "young cat looking up at camera"
+      />
+      <img
+      height = {200}
+      src = {require("./assets/images/cute-kittens2.jpeg")}
+      alt = "cute kitten, laying on it's back"
+      />
+      <img
+      src = {randomImageUrl}
+      height = {200}
+      alt = "some random pic from the internet"
+      />
     </div>
   );
 }
